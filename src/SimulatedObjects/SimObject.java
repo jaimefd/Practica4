82d348fc81172bf133a65c8b51a5558fec4b22ce
jaimefd <skyjimmy7@gmail.com>
2018-03-01 17:07:44 +0100

    funcion report

--- src/SimulatedObjects/SimObject.java
+++ src/SimulatedObjects/SimObject.java
@@ -1,9 +1,29 @@
 package SimulatedObjects;
 
-public class SimObject {
-	String id;
+import java.util.Map;
+
+public abstract class SimObject {
+	protected String id;
 	
 	public String getID(){
 		return id;
 	}
+	
+	public abstract void avanza();
+	
+	protected abstract void fillReportDetails(Map<String, String> out);
+	
+	protected abstract String getReportHeader(); 
+	
+	public static boolean equals(SimObject a, SimObject b){
+		return a.getID() == b.getID();
+	}
+	
+	public void report(int time, Map<String, String> m) {
+		m.put("", getReportHeader());
+		m.put("id", id);
+		m.put("time", String.valueOf(time));
+		fillReportDetails(m);
+		}
+
 }

db392957d0caa2e8c6911921b198211239e17b2e
jaimefd <skyjimmy7@gmail.com>
2018-03-01 18:01:44 +0100

    Implementando los builders (WIP)

--- src/Events/NewJunctionEventBuilder.java
+++ src/Events/NewJunctionEventBuilder.java
@@ -2,9 +2,21 @@ package Events;
 
 import es.ucm.fdi.ini.IniSection;
 
-public class NewJunctionEventBuilder {
-	public static Event parse(IniSection sec) {
+public class NewJunctionEventBuilder implements EventBuilder {
+	public Event parse(IniSection sec) {
 		if (!sec.getTag().equals("new_junction")) return null;
-		return new NewJunctionEvent());
+		return new NewJunctionEvent(Integer.parseInt(sec.getValue("time")), sec.getValue("id"));
+	}
+	
+	public boolean isValidId(String id){
+		return id.charAt(0) == 'j';
+	}
+	
+	public void parseInt(IniSection sec, String key, int de){
+		
+	}
+	
+	public void parseIdList(IniSection sec, String key){
+		
 	}
 }

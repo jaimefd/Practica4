e4bd7c7ba71c3469f656390d1d6199ed6900ab1c
MBrianLC <mbrianlc@gmail.com>
2018-03-01 21:32:55 +0100

    Commit

--- src/Simulator/RoadMap.java
+++ src/Simulator/RoadMap.java
@@ -1,9 +1,13 @@
 package Simulator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import SimulatedObjects.Junction;
+import SimulatedObjects.Road;
 import SimulatedObjects.SimObject;
+import SimulatedObjects.Vehicle;
 
 public class RoadMap {
 
@@ -15,6 +19,8 @@ public class RoadMap {
 	private List<Vehicle> vehicles = new ArrayList<>();
 	// listados read-only, via Collections.unmodifiableList();
 	private List<Junction> junctionsRO;
-	private List<Road> junctionsRO;
-	private List<Vehicle> junctionsRO; 
+	private List<Road> roadsRO;
+	private List<Vehicle> vehiclesRO; 
+	
+	
 }

521ef2c5975912552fb14ee48823467ffb1cef47
jaimefd <skyjimmy7@gmail.com>
2018-03-11 18:33:30 +0100

    Adding VehicleFaultyBuilder class and method parseInt to all builders.

--- src/Events/NewRoadEventBuilder.java
+++ src/Events/NewRoadEventBuilder.java
@@ -5,17 +5,17 @@ import es.ucm.fdi.ini.IniSection;
 public class NewRoadEventBuilder implements EventBuilder {
 	public Event parse(IniSection sec) {
 		if (!sec.getTag().equals("new_road")) return null;
-		return new NewRoadEvent(Integer.parseInt(sec.getValue("time")), sec.getValue("id"), 
+		return new NewRoadEvent(parseInt(sec, "time"), sec.getValue("id"), 
 				sec.getValue("src"), sec.getValue("dest"),
-				sec.getValue("max_speed"), sec.getValue("length"));
+				parseInt(sec, "max_speed"), parseInt(sec, "length"));
 	}
 	
 	public boolean isValidId(String id){
 		return id.charAt(0) == 'r';
 	}
 	
-	public void parseInt(IniSection sec, String key, int de){
-		
+	public int parseInt(IniSection sec, String key){
+		return Integer.parseInt(sec.getValue(key));
 	}
 	
 	public void parseIdList(IniSection sec, String key){

68a7df3080297021ce28062ee91a3b3f4091825a
MBrianLC <mbrianlc@gmail.com>
2018-03-15 18:10:44 +0100

    AdvancedObjects (3)

--- p4_brian_jaime/src/main/java/es/ucm/fdi/AdvancedObjects/Dirt.java
+++ p4_brian_jaime/src/main/java/es/ucm/fdi/AdvancedObjects/Dirt.java
@@ -42,9 +42,14 @@ public class Dirt extends Road{
 	 */
 	
 	public void avanza(){
-		int velBase = Math.min(maxVel, maxVel / Math.max(vehiculos.size(), 1) + 1);
+		int velBase = Math.min(maxVel, maxVel / Math.max((int) vehiculos.sizeOfValues(), 1) + 1);
 		int factorRed = 1;
 		MultiTreeMap<Integer, Vehicle> map = new MultiTreeMap<>((a, b) -> a - b);
+		if (vehiculos.containsKey(longitud)){
+			for(Vehicle v: vehiculos.get(longitud)){
+				map.putValue(longitud, v);
+			}
+		}
 		for (int i = longitud - 1; i >= 0; --i){
 			if (vehiculos.containsKey(i)){
 				for(Vehicle v: vehiculos.get(i)){
@@ -55,8 +60,13 @@ public class Dirt extends Road{
 		for (int i = longitud - 1; i >= 0; --i){
 			if (vehiculos.containsKey(i)){
 				for(Vehicle v: vehiculos.get(i)){
-					v.setVelocidadActual(velBase / factorRed);
+					if (v.getAveria()) v.setVelocidadActual(0);
+					else v.setVelocidadActual(velBase / factorRed);
 					v.avanza();
+					if (v.getPos() == longitud) {
+						v.setVelocidadActual(0);
+						getQueue().add(v);
+					}
 					map.putValue(v.getPos(), v);
 				}
 			}
--- p4_brian_jaime/src/main/java/es/ucm/fdi/AdvancedObjects/Lane.java
+++ p4_brian_jaime/src/main/java/es/ucm/fdi/AdvancedObjects/Lane.java
@@ -46,9 +46,14 @@ public class Lane extends Road{
 	 */
 	
 	public void avanza(){
-		int velBase = Math.min(maxVel, maxVel * numCarriles / Math.max(vehiculos.size(), 1) + 1);
+		int velBase = Math.min(maxVel, maxVel * numCarriles / Math.max((int) vehiculos.sizeOfValues(), 1) + 1);
 		int factorRed = 1, numObstaculos = 0;
 		MultiTreeMap<Integer, Vehicle> map = new MultiTreeMap<>((a, b) -> a - b);
+		if (vehiculos.containsKey(longitud)){
+			for(Vehicle v: vehiculos.get(longitud)){
+				map.putValue(longitud, v);
+			}
+		}
 		for (int i = longitud - 1; i >= 0; --i){
 			if (vehiculos.containsKey(i)){
 				for(Vehicle v: vehiculos.get(i)){
@@ -66,8 +71,13 @@ public class Lane extends Road{
 		for (int i = longitud - 1; i >= 0; --i){
 			if (vehiculos.containsKey(i)){
 				for(Vehicle v: vehiculos.get(i)){
-					v.setVelocidadActual(velBase / factorRed);
+					if (v.getAveria()) v.setVelocidadActual(0);
+					else v.setVelocidadActual(velBase / factorRed);
 					v.avanza();
+					if (v.getPos() == longitud) {
+						v.setVelocidadActual(0);
+						getQueue().add(v);
+					}
 					map.putValue(v.getPos(), v);
 				}
 			}
